185757a5b32b1d7f77ba2fff36ad61b6beaf8931
GreenAlien <edvardaskerikas@gmail.com>
2025-04-02 15:48:18 +0300

    Nd

--- js/99-ND.js
+++ js/99-ND.js
@@ -11,6 +11,15 @@ console.log(sum)
 
 //2
 
-for (let i = 1; i <= 1; i++) {
-    console.log('Vakaras');
+for (let i = 0; i < "Vakaras".length;  i++) {
+    console.log("Vakaras"[i]);
 }
+
+
+//3
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`6 * ${i} = ${6 * i}`);
+}
+
+console.clear();
